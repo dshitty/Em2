eabec3a88736307c4f0c1b9f7cf3e35b5c2e04ee
Darshan <chhantyaldarshan2@gmail.com>
2026-02-06 15:28:29 +0545

    4th commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
 // import LoginPage from "./components/LoginPage"
-// import LoginPage2 from "./components/LoginPage2"
-import RespLoginPage from "./components/RespLoginPage"
+import LoginPage2 from "./components/LoginPage2";
+// import RespLoginPage from "./components/RespLoginPage"
 
-const App = ()=>{
-return(
+const App = () => {
+  return (
     <>
-    
-    {/* <LoginPage2/> */}
-    <RespLoginPage/>
-    
+      <LoginPage2 />
+      {/* <RespLoginPage/> */}
     </>
-    
-
-)
-}
-export default App
+  );
+};
+export default App;
