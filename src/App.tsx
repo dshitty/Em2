87f0dd1e31098a719b1eea278498515f3a93b4a7
Darshan <chhantyaldarshan2@gmail.com>
2026-02-09 11:07:47 +0545

    2nd commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 // import LoginPage from "./components/LoginPage"
 // import LoginPage2 from "./components/LoginPage2";
 // import RespLoginPage from "./components/RespLoginPage"
-
-import LoginSignup from "./components/LoginSignup";
 import Test from "./components/test";
 
 const App = () => {
