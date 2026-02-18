2ffa2e4c18d314d93978c4eb3e586dc314345a13
Darshan <chhantyaldarshan2@gmail.com>
2026-02-18 09:01:25 +0545

    1st commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,28 @@
-import LoginPage from "./components/LoginPage"
+// import LoginPage from "./components/LoginPage"
 // import LoginPage2 from "./components/LoginPage2";
 // import RespLoginPage from "./components/RespLoginPage"
-// import Test from "./components/Login3";
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Test from "./pages/Login3";
+import Dashboard1 from "./pages/Dashboard1";
+// import Map from "./component/Map";
+// import Lottie1 from "./components/Lottie1";
+// import MusicGame from "./component2/MusicGame";
 const App = () => {
   return (
     <>
+    
+   <BrowserRouter>
+   <Routes>
+   <Route path="/dashboard" element={<Dashboard1/>}/>
+   <Route path="/" element={<Test/>}/>
+   </Routes>
       {/* <LoginPage2 /> */}
       {/* <RespLoginPage/> */}
       {/* <LoginSignup/> */}
-      {/* <Test/> */}
-      <LoginPage/>
+      
+      {/* <LoginPage/> */}
+      {/* <MusicGame/> */}
+   </BrowserRouter>
     </>
   );
 };
---
+++ src/component/Map.tsx
@@ -0,0 +1,104 @@
+import React, { useEffect, useState } from 'react';
+import { MapContainer, TileLayer, Marker, Popup, Circle } from 'react-leaflet';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+
+// Fix marker icon issue in Leaflet + React
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
+
+// TypeScript interface
+interface Hospital {
+  id: number;
+  lat: number;
+  lng: number;
+  name: string;
+}
+
+const Map: React.FC = () => {
+  const [position, setPosition] = useState<[number, number] | null>(null);
+  const [hospitals, setHospitals] = useState<Hospital[]>([]);
+
+  // Get user location
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (pos) => setPosition([pos.coords.latitude, pos.coords.longitude]),
+      (err) => {
+        console.error(err);
+        alert('Please allow location access');
+      }
+    );
+  }, []);
+
+  // Fetch nearby hospitals from Overpass API
+  useEffect(() => {
+    if (!position) return;
+
+    const fetchHospitals = async () => {
+      try {
+        const [lat, lng] = position;
+        const radius = 3000; // in meters (3km)
+        const overpassQuery = `
+          [out:json];
+          node(around:${radius},${lat},${lng})["amenity"="hospital"];
+          out;
+        `;
+        const url = `https://overpass-api.de/api/interpreter?data=${encodeURIComponent(overpassQuery)}`;
+        const response = await fetch(url);
+        const data = await response.json();
+
+        // Map data to Hospital type
+        const mapped: Hospital[] = data.elements.map((el: any) => ({
+          id: el.id,
+          lat: el.lat,
+          lng: el.lon,
+          name: el.tags?.name || 'Unknown Hospital',
+        }));
+
+        setHospitals(mapped);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchHospitals();
+  }, [position]);
+
+  if (!position) return <div>Loading your location...</div>;
+
+  return (
+    
+      <MapContainer
+        center={position}
+        zoom={14}
+        style={{ width: '100%', height: '100%' }}
+      >
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+
+        {/* User location */}
+        <Marker position={position}>
+          <Popup>Your location</Popup>
+        </Marker>
+
+        {/* Accuracy circle */}
+        <Circle center={position} radius={50} pathOptions={{ color: 'blue', fillColor: 'blue', fillOpacity: 0.2 }} />
+
+        {/* Hospital markers */}
+        {hospitals.map((h) => (
+          <Marker key={h.id} position={[h.lat, h.lng]}>
+            <Popup>{h.name}</Popup>
+          </Marker>
+        ))}
+      </MapContainer>
+  );
+};
+
+export default Map;
